11ba4bd2b0927029a3babc2e5b7ae310f568b43e
Joha Sam <johasam@Johas-MacBook-Air-3.local>
2026-04-17 21:30:52 -0400

    fix channel

--- controller.py
+++ controller.py
@@ -24,7 +24,7 @@ import board
 import busio
 import neopixel
 import adafruit_ads1x15.ads1115 as ADS
-from adafruit_ads1x15.analog_in import AnalogIn
+from adafruit_ads1x15.ads1115 import ADS1115
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -94,7 +94,7 @@ def init_hardware():
     """
     i2c = busio.I2C(board.SCL, board.SDA)
     ads = ADS.ADS1115(i2c)
-    chan = AnalogIn(ads, ADS.P0)
+    chan = AnalogIn(ads, 0)
 
     pixels = neopixel.NeoPixel(
         NEOPIXEL_PIN,
